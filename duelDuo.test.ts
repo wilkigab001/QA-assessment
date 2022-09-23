ef9ace6164afe2db8291e3f3121512b2973cecdd
Gabriel Wilkins <wilkigab001@gmail.com>
2022-09-23 12:49:32 -0600

    Please work

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -19,19 +19,19 @@ test('Title shows up when page loads', async () => {
     expect(displayed).toBe(true)
 })
 
-// test('Checks that cklicking the draw button displays the div with id of choices', async() => {
-//     const drawBtn = await driver.findElement(By.id('draw')).click()
-//     await driver.sleep(2000)
+test('Checks that cklicking the draw button displays the div with id of choices', async() => {
+    const drawBtn = await driver.findElement(By.id('draw')).click()
+    await driver.sleep(2000)
 
-//     const choicesDiv = await driver.findElement(By.xpath('//div[contains(text(), "")]')).click()
-//     await driver.sleep(2000)
-//     const isShown = choicesDiv.isDisplayed()
-//     expect(isShown).toBe(true)
-// })
+    const choicesDiv = await driver.findElement(By.xpath('//div[contains(text(), "")]')).click()
+    await driver.sleep(2000)
+    const isShown = choicesDiv.isDisplayed()
+    expect(isShown).toBe(true)
+})
 
-// test('Check that clicking an “Add to Duo” button displays the div with id = “player-duo”', async () => {
-//     const drawBtn = await driver.findElement(By.id('draw')).click()
-//     await driver.sleep(2000)
+test('Check that clicking an “Add to Duo” button displays the div with id = “player-duo”', async () => {
+    const drawBtn = await driver.findElement(By.id('draw')).click()
+    await driver.sleep(2000)
 
-//     const addBotBtn = await driver.findElement(By.css
-// })
+    const addBotBtn = await driver.findElement(By.css
+})
